58e1cd3b1960d46c09dd72306ec9319943b8674c
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2021-01-27 23:27:56 -0300

    Edit Game Screen

---
+++ app/src/main/java/com/example/desafio_firebase/EditGameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.desafio_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_game)
+    }
+}
--- app/src/main/java/com/example/desafio_firebase/MainActivity.kt
+++ app/src/main/java/com/example/desafio_firebase/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.desafio_firebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.desafio_firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.floatingActionButton.setOnClickListener {
+            val i = Intent(this@MainActivity, EditGameActivity::class.java)
+            startActivity(i)
+        }
     }
 }
